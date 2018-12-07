2deb274211903949a1bd7eeba6d33c175dd77e8e
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2018-12-07 10:55:39 +0900

    add Akka HTTP Main method

---
+++ src/main/scala/example/myapp/helloworld/GreeterServer.scala
@@ -0,0 +1,46 @@
+package example.myapp.helloworld
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.UseHttp2.Always
+import akka.http.scaladsl.{Http, HttpConnectionContext}
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.stream.{ActorMaterializer, Materializer}
+import com.typesafe.config.ConfigFactory
+import example.myapp.helloworld.grpc.GreeterServiceHandler
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object GreeterServer extends App {
+
+  val conf = ConfigFactory
+    .parseString("akka.http.server.preview.enable-http2 = on")
+    .withFallback(ConfigFactory.defaultApplication())
+  val system = ActorSystem("HelloWorld", conf)
+
+  new GreeterServer(system).run()
+}
+
+class GreeterServer(system: ActorSystem) {
+
+  def run(): Future[Http.ServerBinding] = {
+    implicit val sys: ActorSystem = system
+    implicit val mat: Materializer = ActorMaterializer()
+    implicit val ec: ExecutionContext = system.dispatcher
+
+    val service: HttpRequest => Future[HttpResponse] =
+      GreeterServiceHandler(new GreeterServiceImpl(mat))
+
+    val bound = Http().bindAndHandleAsync(
+      service,
+      interface = "127.0.0.1",
+      port = 8080,
+      connectionContext = HttpConnectionContext(http2 = Always)
+    )
+
+    bound.foreach { binding =>
+      println(s"gRPC server bound to: ${binding.localAddress}")
+    }
+
+    bound
+  }
+}
