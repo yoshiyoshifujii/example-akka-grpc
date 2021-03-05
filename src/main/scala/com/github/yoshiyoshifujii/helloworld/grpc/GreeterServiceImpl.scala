9b6edef0595e239b151dfc1ac63b80594b58f804
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-03-05 21:29:09 +0900

    add proto and Service impl

---
+++ src/main/scala/com/github/yoshiyoshifujii/helloworld/grpc/GreeterServiceImpl.scala
@@ -0,0 +1,31 @@
+package com.github.yoshiyoshifujii.helloworld.grpc
+import akka.NotUsed
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Sink, Source}
+import com.google.protobuf.timestamp.Timestamp
+
+import scala.concurrent.Future
+
+class GreeterServiceImpl(implicit mat: Materializer) extends GreeterService {
+  import mat.executionContext
+
+  override def sayHello(in: HelloRequest): Future[HelloReply] = {
+    println(s"sayHello to ${in.name}")
+    Future.successful(HelloReply(s"Hello, ${in.name}", Some(Timestamp.apply(123456, 123))))
+  }
+
+  override def itKeepsTalking(in: Source[HelloRequest, NotUsed]): Future[HelloReply] = {
+    println(s"sayHello to in stream...")
+    in.runWith(Sink.seq).map(elements => HelloReply(s"Hello, ${elements.map(_.name).mkString(",")}"))
+  }
+
+  override def itKeepsReplying(in: HelloRequest): Source[HelloReply, NotUsed] = {
+    println(s"sayHello to ${in.name} with stream of charts...")
+    Source(s"Hello, ${in.name}".toList).map(character => HelloReply(character.toString))
+  }
+
+  override def streamHellos(in: Source[HelloRequest, NotUsed]): Source[HelloReply, NotUsed] = {
+    println(s"sayHello to stream...")
+    in.map(request => HelloReply(s"Hello, ${request.name}"))
+  }
+}
